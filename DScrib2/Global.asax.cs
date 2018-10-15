8c5da69452042348c6421afc3674ad438d7bf52e
Michael Rivera <soymrmike@gmail.com>
2018-10-14 22:49:25 -0700

    Setup a JSON response for index.html.

--- DScrib2/Global.asax.cs
+++ DScrib2/Global.asax.cs
@@ -4,25 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
-using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace DScrib2
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
-
     public class Global : System.Web.HttpApplication
     {
 
---
+++ DScrib2/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DScrib2
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string q)
+        {
+            var results = new List<string>() { $"fake{q}result1", "result2", "result3" };
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
