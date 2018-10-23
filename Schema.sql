395996157e5c0636b895681905ea496f95e0370a
Michael Rivera <soymrmike@gmail.com>
2018-10-23 00:38:21 -0700

    Experimental schema for relating users to reviews.

---
+++ Schema.sql
@@ -0,0 +1,36 @@
+﻿
+-- create DScrib2 database, first.
+
+-- You can create a login with this.
+--USE [master]
+--GO
+--CREATE LOGIN [dscrib2dev] WITH PASSWORD=N'backintime', DEFAULT_DATABASE=[DScrib2], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+--GO
+
+--USE [DScrib2]
+--GO
+--CREATE USER [dscrib2dev] FOR LOGIN [dscrib2dev]
+--GO
+--USE [DScrib2]
+--GO
+--ALTER USER [dscrib2dev] WITH DEFAULT_SCHEMA=[dbo]
+--GO
+
+--USE [DScrib2]
+--GO
+--EXEC sp_addrolemember N'db_owner', N'dscrib2dev'
+--GO
+
+
+-- Schema.
+CREATE TABLE "User" (
+  "ID" INT PRIMARY KEY,
+  "Email" NVARCHAR,
+  "VendorID" NVARCHAR,
+  );
+
+CREATE TABLE "Reviews" (
+  "ID" INT PRIMARY KEY,
+  "Text" NVARCHAR,
+  "UserID" INT FOREIGN KEY REFERENCES "User"(ID)
+);
