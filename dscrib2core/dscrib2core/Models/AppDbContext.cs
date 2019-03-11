6487e0c48d57aa7950c63c4b04b4ec3bd6cb0830
Michael Rivera <soymrmike@gmail.com>
2019-03-11 16:43:59 -0700

    DbContext tied to db with connection string in OnConfiguring.

--- dscrib2core/dscrib2core/Models/AppDbContext.cs
+++ dscrib2core/dscrib2core/Models/AppDbContext.cs
@@ -9,6 +9,11 @@ namespace DScrib2.Models
 {
     public class AppDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DScrib2;Trusted_Connection=True;");
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Review> Reviews { get; set; }
     }
